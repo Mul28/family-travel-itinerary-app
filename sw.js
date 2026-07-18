0678f6a7ecd5b0d7bf45e2d3cdba56853ddaf357
Gavin Muldowney <gavinmuldowney@yahoo.co.uk>
2026-07-18 19:29:07 +1000

    Add Caserta flight-day guide

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "family-journey-v4";
+const CACHE_NAME = "family-journey-v5";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
